f9f0f613b84128fdc99d48d8e43985f2f30b4d6d
lebarquero <lebarquero@gmail.com>
2023-07-24 15:00:57 -0600

    Add generic repository

---
+++ BankingAPI/DataAccess/Repositories/IRepositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace BankingAPI.DataAccess.Repositories.IRepositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter, bool tracked = true);
+        Task CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task SaveAsync();
+    }
+}
---
+++ BankingAPI/DataAccess/Repositories/Repository.cs
@@ -0,0 +1,38 @@
+﻿using BankingAPI.DataAccess.Repositories.IRepositories;
+using System.Linq.Expressions;
+
+namespace BankingAPI.DataAccess.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public Task CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(Expression<Func<T, bool>> filter, bool tracked = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
