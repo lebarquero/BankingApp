df7b1f23f30b67caf6f3cb88e115e9ae56d312d7
lebarquero <lebarquero@gmail.com>
2023-07-24 16:18:20 -0600

    Add Mapping profile and configure it in Service container

---
+++ BankingAPI/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BankingAPI.DTOs.Cliente;
+using BankingAPI.Entities;
+
+namespace BankingAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Cliente, ClienteCreateDTO>().ReverseMap();
+            CreateMap<Cliente, ClienteUpdateDTO>().ReverseMap();
+            CreateMap<Cliente, ClienteDTO>().ReverseMap();
+        }
+    }
+}
