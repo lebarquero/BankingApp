80e1704379f046614ccb294a7d2c4fc904222c73
lebarquero <lebarquero@gmail.com>
2023-07-24 14:39:49 -0600

    Add migrations

---
+++ BankingAPI/Migrations/20230724203533_CreateDbBanking.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDbBanking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Contrasena = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Genero = table.Column<int>(type: "int", nullable: true),
+                    Edad = table.Column<int>(type: "int", nullable: true),
+                    Identificacion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
