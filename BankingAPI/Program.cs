8585d9f49e1c2d05ba5fae4630e0d7f4f41a1454
lebarquero <lebarquero@gmail.com>
2023-07-24 14:20:54 -0600

    Configure DbContext service container

--- BankingAPI/Program.cs
+++ BankingAPI/Program.cs
@@ -1,6 +1,13 @@
+using BankingAPI.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<BankingDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BankingConnection"));
+});
 
 builder.Services.AddControllers();
 
