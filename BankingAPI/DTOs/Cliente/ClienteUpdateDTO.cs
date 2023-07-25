444f7ef29b803b0c7b36c94aeec9abaff2b5fcf7
lebarquero <lebarquero@gmail.com>
2023-07-25 14:38:23 -0600

    Add error messages to ClienteUpdateDTO

--- BankingAPI/DTOs/Cliente/ClienteUpdateDTO.cs
+++ BankingAPI/DTOs/Cliente/ClienteUpdateDTO.cs
@@ -8,24 +8,26 @@ namespace BankingAPI.DTOs.Cliente
         [Key]
         public int ID { get; set; }
 
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "La longuitud máxima permitida es de 50 caracteres")]
         public string Nombre { get; set; } = null!;
 
-        [EnumDataType(typeof(Genero))]
+        [EnumDataType(typeof(Genero), ErrorMessage = "Solo se permiten los siguientes valores: Femenino, Masculino")]
         public string Genero { get; set; } = null!;
 
         public int? Edad { get; set; }
 
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "La longuitud máxima permitida es de 50 caracteres")]
         public string? Identificacion { get; set; }
 
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "La longuitud máxima permitida es de 50 caracteres")]
         public string Direccion { get; set; } = null!;
 
-        [StringLength(9)]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "La longuitud debe ser de 9 digitos")]
+        [RegularExpression("^\\d+$", ErrorMessage = "Solo se permiten números")]
         public string Telefono { get; set; } = null!;
 
-        [StringLength(4)]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "La longuitud debe ser de 4 digitos")]
+        [RegularExpression("^\\d+$", ErrorMessage = "Solo se permiten números")]
         public string Contrasena { get; set; } = null!;
 
         public bool Estado { get; set; }
