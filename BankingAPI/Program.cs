9df341c5b6ec7c8e25a9fcb32d12c9b2ab3f2de0
lebarquero <lebarquero@gmail.com>
2023-07-25 13:26:27 -0600

    Add JsonStringEnumConverter to Controllers service

--- BankingAPI/Program.cs
+++ BankingAPI/Program.cs
@@ -4,6 +4,7 @@ using BankingAPI.DataAccess.Repositories;
 using BankingAPI.DataAccess.Repositories.IRepositories;
 using BankingAPI.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +19,8 @@ builder.Services.AddAutoMapper(typeof(MappingConfig));
 
 builder.Services.AddScoped<IRepository<Cliente>, Repository<Cliente>>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
 var app = builder.Build();
 
