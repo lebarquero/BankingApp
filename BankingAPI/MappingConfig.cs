f253d510e642657ad7067201dd0ff623fead02a2
lebarquero <lebarquero@gmail.com>
2023-07-27 09:48:26 -0600

    Add mapping to Cuenta and Movimiento

--- BankingAPI/MappingConfig.cs
+++ BankingAPI/MappingConfig.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using BankingAPI.DTOs.Cliente;
+using BankingAPI.DTOs.Cuenta;
+using BankingAPI.DTOs.Movimiento;
 using BankingAPI.Entities;
 
 namespace BankingAPI
@@ -11,6 +13,14 @@ namespace BankingAPI
             CreateMap<Cliente, ClienteCreateDTO>().ReverseMap();
             CreateMap<Cliente, ClienteUpdateDTO>().ReverseMap();
             CreateMap<Cliente, ClienteDTO>().ReverseMap();
+
+            CreateMap<Cuenta, CuentaCreateDTO>().ReverseMap();
+            CreateMap<Cuenta, CuentaUpdateDTO>().ReverseMap();
+            CreateMap<Cuenta, CuentaDTO>().ReverseMap();
+
+            CreateMap<Movimiento, MovimientoCreateDTO>().ReverseMap();
+            CreateMap<Movimiento, MovimientoUpdateDTO>().ReverseMap();
+            CreateMap<Movimiento, MovimientoDTO>().ReverseMap();
         }
     }
 }
