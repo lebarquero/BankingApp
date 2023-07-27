2c328e9938232b439300a6ba7ec9dd744148c493
lebarquero <lebarquero@gmail.com>
2023-07-26 22:04:50 -0600

    Add Movimiento DTOs

---
+++ BankingAPI/DTOs/Movimiento/MovimientoCreateDTO.cs
@@ -0,0 +1,23 @@
+﻿using BankingAPI.Entities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankingAPI.DTOs.Movimiento
+{
+    public class MovimientoCreateDTO
+    {
+        [Required(ErrorMessage = "La fecha es requerida")]
+        public DateTime Fecha { get; set; }
+
+        [EnumDataType(typeof(TipoMovimiento), ErrorMessage = "Solo se permiten los siguientes valores: Debito, Credito")]
+        [Required(ErrorMessage = "El tipo de movimiento es requerido")]
+        public string TipoMovimiento { get; set; } = null!;
+
+        [Required(ErrorMessage = "El valor es requerido")]
+        public decimal Valor { get; set; }
+
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "La longuitud del número de cuenta debe ser de digitos")]
+        [Required(ErrorMessage = "El número de cuenta es requerido")]
+        [RegularExpression("^\\d+$", ErrorMessage = "Solo se permiten números")]
+        public string NumeroCuenta { get; set; } = null!;
+    }
+}
---
+++ BankingAPI/DTOs/Movimiento/MovimientoDTO.cs
@@ -0,0 +1,19 @@
+﻿using BankingAPI.Entities.Enums;
+
+namespace BankingAPI.DTOs.Movimiento
+{
+    public class MovimientoDTO
+    {
+        public int MovimientoID { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public TipoMovimiento TipoMovimiento { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public decimal Saldo { get; set; }
+
+        public string NumeroCuenta { get; set; } = null!;
+    }
+}
