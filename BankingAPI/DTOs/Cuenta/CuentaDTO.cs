4f74e40b0f51aaf2e413d2642433e7a763d42d86
lebarquero <lebarquero@gmail.com>
2023-07-26 20:51:59 -0600

    Add Cuenta DTOs

---
+++ BankingAPI/DTOs/Cuenta/CuentaDTO.cs
@@ -0,0 +1,19 @@
+﻿using BankingAPI.Entities.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankingAPI.DTOs.Cuenta
+{
+    public class CuentaDTO
+    {
+        public string NumeroCuenta { get; set; } = null!;
+
+        public TipoCuenta TipoCuenta { get; set; }
+
+        public decimal SaldoInicial { get; set; }
+
+        public bool Estado { get; set; }
+
+        public int ClienteID { get; set; }
+    }
+}
---
+++ BankingAPI/DTOs/Cuenta/CuentaUpdateDTO.cs
@@ -0,0 +1,26 @@
+﻿using BankingAPI.Entities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankingAPI.DTOs.Cuenta
+{
+    public class CuentaUpdateDTO
+    {
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "La longuitud del número de cuenta debe ser de digitos")]
+        [Required(ErrorMessage = "El número de cuenta es requerido")]
+        [RegularExpression("^\\d+$", ErrorMessage = "Solo se permiten números")]
+        public string NumeroCuenta { get; set; } = null!;
+
+        [Required(ErrorMessage = "El tipo de cuenta es requerido")]
+        [EnumDataType(typeof(TipoCuenta), ErrorMessage = "Solo se permiten los siguientes valores: Ahorro, Corriente")]
+        public string TipoCuenta { get; set; } = null!;
+
+        [Required(ErrorMessage = "El saldo inicial es requerido")]
+        public decimal SaldoInicial { get; set; }
+
+        [Required(ErrorMessage = "El estado es requerido")]
+        public bool Estado { get; set; }
+
+        [Required(ErrorMessage = "El cliente es requerido")]
+        public int ClienteID { get; set; }
+    }
+}
